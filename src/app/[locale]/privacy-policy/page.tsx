bb55b94d4073906ae02cbbb5b48e22cf85f89606
Ali Samir <alisamir.eng@gmail.com>
2024-01-27 19:51:52 +0200

    dev

---
+++ src/app/[locale]/privacy-policy/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import I18n from "../../[locale]/privacy-policy";
+
+interface Translations {
+  [language: string]: {
+    greeting: string;
+  };
+}
+
+// const translations: Translations =
+
+export default function PrivacyPolicy() {
+  return <h1>Privacy Policy</h1>;
+}
